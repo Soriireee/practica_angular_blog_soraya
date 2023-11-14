d4f128c2d923480871cba2e26c79377059e53962
Soraya Ramos <soraya.210836@gmail.com>
2023-11-14 15:03:08 +0100

    creados todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ListaPostsComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
