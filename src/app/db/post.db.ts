78f6c25ea89a1a0ab8212975fdcfb78e4484e4ca
Soraya Ramos <soraya.210836@gmail.com>
2023-11-20 23:03:53 +0100

    array de datos

---
+++ src/app/db/post.db.ts
@@ -0,0 +1,29 @@
+import { Post } from '../interfaces/post.interface';
+
+export const POSTS: Post[] = [
+    {
+        id: 1, titulo: 'Viaje a las Montañas', texto: 'Explorando las impresionantes montañas y disfrutando de la naturaleza en su máxima expresión. Un viaje inolvidable lleno de aventuras.', autor: 'Ana García', imagen: 'https://viajes.nationalgeographic.com.es/medio/2019/01/30/lago-britches-italia_e60b11a3_1200x630.jpg', fecha: '2023-04-20', categoria: 'Viajes'
+    },
+    {
+        id: 2, titulo: 'Recetas Saludables: Ensalada de Verano', texto: 'Descubre una deliciosa receta de ensalada de verano llena de ingredientes frescos y sabores vibrantes. Perfecta para una comida ligera y nutritiva.', autor: 'Pedro López', imagen: 'https://www.pequerecetas.com/wp-content/uploads/2019/04/ensaladas-de-verano-recetas.jpeg', fecha: '2023-05-05', categoria: 'Cocina'
+    },
+    {
+        id: 3, titulo: 'Últimas Tendencias en Moda Primavera-Verano', texto: 'Exploramos las últimas tendencias de moda para la temporada primavera-verano. Consejos sobre estilos, colores y accesorios de moda.', autor: 'Elena Martínez', imagen: 'https://img.freepik.com/vector-premium/conjunto-ropa-mujer-joven-botas-bolso-jeans-chaqueta-mezclilla-conjunto-colores-moda-primavera_619584-46.jpg?w=2000',
+        fecha: '2023-06-15', categoria: 'Moda'
+    },
+    {
+        id: 4, titulo: 'Consejos para un Estilo de Vida Activo', texto: 'Descubre cómo mantener un estilo de vida activo con simples consejos y rutinas de ejercicio. La clave para mantenerse en forma y saludable.', autor: 'Martín Sánchez', imagen: 'https://www.valgo.es/uploads/app/163/elements/1/904image1617784032o.jpg', fecha: '2023-07-10', categoria: 'Salud y Bienestar'
+    },
+    {
+        id: 5, titulo: 'Guía de Fotografía para Principiantes', texto: 'Aprende los conceptos básicos de la fotografía, desde la elección de la cámara hasta la composición de la imagen. Consejos para capturar momentos inolvidables.', autor: 'Laura Torres', imagen: 'https://www.dzoom.org.es/wp-content/uploads/2020/01/falsos-mitos-fotografia-equipo-camara-tripode-estabilidad-734x489.jpg', fecha: '2023-08-02', categoria: 'Fotografía'
+    },
+    {
+        id: 7, titulo: 'Decoración del Hogar: Estilo Minimalista', texto: 'Consejos y trucos para lograr una decoración minimalista en tu hogar. Cómo simplificar espacios y crear un ambiente tranquilo y armonioso.', autor: 'Sofía Fernández', imagen: 'https://images.hola.com/imagenes/decoracion/20200317163208/ideas-decoracion-casa/0-798-675/aislamiento-casa-8t-t.jpg?tx=w_680', fecha: '2023-10-05', categoria: 'Hogar'
+    },
+    {
+        id: 8, titulo: 'Rutina de Cuidado de la Piel para Todas las Edades', texto: 'Una rutina completa de cuidado de la piel para mantener una piel saludable en todas las etapas de la vida. Productos recomendados y consejos de expertos.', autor: 'Alejandra Díaz', imagen: 'https://www.pmfarma.com/noticias/noticias/48865/image/cuidado%20piel%20merz.jpg', fecha: '2023-11-15', categoria: 'Belleza'
+    },
+    {
+        id: 9, titulo: 'Relax en las Playas de Bali', texto: 'Disfrutando del sol, arena y aguas cristalinas en las hermosas playas de Bali. Un destino paradisíaco para descansar y relajarse.', autor: 'Elena Martínez', imagen: 'https://mochilerosentailandia.com/wp-content/uploads/2016/01/1-5.jpg', fecha: '2023-10-05', categoria: 'Viajes'
+    },
+];
