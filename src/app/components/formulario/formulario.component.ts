76b5b0842dbe1f7ba00faba730261bc7fc57c620
Soraya Ramos <soraya.210836@gmail.com>
2023-11-20 22:53:54 +0100

    creado formulario en html y css - funcion on submit en ts

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  formulario: FormGroup;
+  PostsService = inject(PostService);
+
+  constructor() {
+    this.formulario = new FormGroup({
+      titulo: new FormControl(null,
+        [Validators.required]
+      ),
+      texto: new FormControl(),
+      autor: new FormControl(),
+      imagen: new FormControl(),
+      fecha: new FormControl(),
+      categorias: new FormControl(),
+    });
+  }
+
+  onSubmit() {
+    this.PostsService.create(this.formulario.value)
+    /* console.log(this.formulario.value); */
+  }
+
+  checkError(controlTitulo: string, errorTitulo: string) {
+    return this.formulario.get(controlTitulo)?.hasError(errorTitulo) && this.formulario.get(controlTitulo)?.touched;
+  }
 }
+
