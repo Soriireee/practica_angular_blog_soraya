e6c6fbe8090050933292efba4593ab34b0bf5237
Soraya Ramos <soraya.210836@gmail.com>
2023-11-20 22:52:31 +0100

    creadas las rutas de home, lista, formulario y detalle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { DetallePostComponent } from './pages/detalle-post/detalle-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'post', component: ListaPostsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'new', component: FormularioComponent },
+  { path: 'detalle/:postId', component: DetallePostComponent },
+  { path: '**', redirectTo: '/home' }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
