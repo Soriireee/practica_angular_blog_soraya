0360d3e523f65e99028d508a62d9615ae0e602b1
Soraya Ramos <soraya.210836@gmail.com>
2023-11-20 22:56:36 +0100

    creado html de lista post con la funcion getAll- getCategorias - getByCategoria en ts

--- src/app/components/lista-posts/lista-posts.component.ts
+++ src/app/components/lista-posts/lista-posts.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/interfaces/post.interface';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-lista-posts',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class ListaPostsComponent {
 
+  arrPosts: Post[] = [];
+  arrCategorias: string[] = [];
+
+  PostsService = inject(PostService);
+
+  ngOnInit() {
+    this.arrPosts = this.PostsService.getAll();
+    this.arrCategorias = this.PostsService.getCategorias();
+  }
+
+  onChange($event: any) {
+    this.arrPosts = this.PostsService.getByCategoria($event.target.value);
+  }
 }
+
+
